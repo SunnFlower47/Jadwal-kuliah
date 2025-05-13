1f0f6dc6ffe95fe6f9b5cb0f32ab06143902cc46
Ridwan Andrian <ridwanandrian2004@gmail.com>
2025-05-14 00:50:49 +0700

    Update welcome.blade.php
    welcome
    
    Co-Authored-By: R37pull <211547506+r37pull@users.noreply.github.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,6 +50,12 @@
             @endif
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+            <div class="flex flex-col items-center justify-center w-full max-w-[335px] lg:max-w-4xl">
+                <img src="{{ asset('images/laravel-logo.png') }}" alt="Laravel Logo" class="w-24 h-24 mb-4">
+                <h1 class="text-2xl font-bold text-[#1b1b18] dark:text-[#EDEDEC]">Laravel</h1>
+                <p class="text-sm text-[#706f6c] dark:text-[#A1A09A]">The PHP Framework for Web Artisans</p>
+            </div>
+        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <h1 class="mb-1 font-medium">Let's get started</h1>
